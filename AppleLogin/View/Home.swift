8dc0a56838154eec77e5dbfb4f35d158853d053e
JI SOO <dlwltn815@gmail.com>
2022-05-22 18:06:10 +0900

    Feat: create home view

---
+++ AppleLogin/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  AppleLogin
+//
+//  Created by LeeJiSoo on 2022/05/22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
