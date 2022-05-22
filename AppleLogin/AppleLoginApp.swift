f57015eae4818f7035d431a9e7b0df6188ea77b0
JI SOO <dlwltn815@gmail.com>
2022-05-22 14:21:05 +0900

    Initial Commit

---
+++ AppleLogin/AppleLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginApp.swift
+//  AppleLogin
+//
+//  Created by LeeJiSoo on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleLogin
+//
+//  Created by LeeJiSoo on 2022/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
